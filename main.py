6f8199c26f8a147e98b395fd11942b6092e7ee9c
HirudineeADRT <hirudinee@adroitlogic.com>
2019-09-20 11:47:46 +0530

    storage py

---
+++ main.py
@@ -0,0 +1,4 @@
+# This file is used to register all your cloud functions in GCP.
+# DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+from hiru2009project1.uploader import handler as hiru2009project1uploader
